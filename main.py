f65d2ce0420c2b669b0ace5f9a2249cbdb56668b
ash <ash@kali.localkali>
2023-05-21 17:43:56 -0300

    att - Links

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 """
 LINKS PARA OS ESTUDOS DE CADA DIA:
 
-    DIA1: https://github.com/CamiloCCarvalho/python/blob/master/studies/day1/main.py
-    DIA2: https://github.com/CamiloCCarvalho/python/blob/master/studies/day2/main.py
-    DIA3: https://github.com/CamiloCCarvalho/python/blob/master/studies/day3/main.py
-    DIA4: https://github.com/CamiloCCarvalho/python/blob/master/studies/day4/main.py
-    DIA5: https://github.com/CamiloCCarvalho/python/blob/master/studies/day5/main.py
-    
-    DIA6 parte1: https://github.com/CamiloCCarvalho/python/blob/master/studies/day6/main.py
-    DIA6 parte2: https://github.com/CamiloCCarvalho/python/blob/master/studies/day6/main2.py
+ No Arquivo README.MD encontra-se todo resumo do conteudo, e também os links para navegação das pastas com conteudo de cada dia.
 
 """
