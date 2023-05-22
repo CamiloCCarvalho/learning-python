b110b29edb00cdcb1ef04eca4bbee9fccb6e534a
ash <ash@kali.localkali>
2023-05-22 09:25:22 -0300

    att

--- exercicies/week1/exMod7.py
+++ exercicies/week1/exMod7.py
@@ -1,2 +1,48 @@
+import random
 # MODULO 7 - EXERCICIO 1
-    # Please wait, loading ...
+
+# LISTA 1 - EX1
+a = [1, 0, 5, -2, -5, 7]
+sumNum = a[0] + a[1] + a[5]
+print(f'a Soma é: {sumNum}')
+
+a[3] = 100
+for index, item in enumerate(a):
+    print(f'index: {index} | valor: {item}')
+
+# LISTA 1 - EX2
+vet = []
+limit = 6
+
+while limit > 0:
+    #vet.append(int(input('Digite um numero inteiro: ')))
+    limit -= 1
+
+print(vet)
+
+# LISTA 1 - EX3
+# elements = []
+# squareElements = []
+# for i in range(10):
+#     elements.append(float(input('Digite um numero Real: ')))
+#     squareElements.append(elements[i]**2)
+# 
+# print(squareElements)
+
+# LISTA 1 - EX4
+random_list = [random.uniform(0, 100) for i in range(8)]
+x = random_list[random.randint(0, 8)]
+y = random_list[random.randint(0, 8)]
+print(f'Lista gerada automaticamente: \n {random_list}')
+print(f'A soma de X e Y é: {x+y}')
+
+# LISTA 1 - EX5
+random_list2 = [random.randint(-100, 100) for i in range(10)]
+odds = 0
+res_list = []
+for item in random_list2:
+    if (item % 2 == 0):
+        odds += 1
+        res_list.append(item)
+print(f'Lista de Pares encontrados: {res_list}')
+print(f'Quantidade de numeros pares: {odds}')
--- studies/week1/day5/main.py
+++ studies/week1/day5/main.py
@@ -6,7 +6,40 @@
 
 
 
-"""MODULO 7 - COLEÇÕES (LISTAS | TUPLAS | DICIONARIOS | MAPAS) - CONTINUAÇÃO"""
+"""MODULO 7 - COLEÇÕES (LISTAS | TUPLAS | DICIONARIOS | MAPAS | CONJUNTOS)"""
+
+# COLEÇÕES SÃO UMA PARTE MUITO IMPORTANTE, POIS É MUITO UTILIZADA EM CIENCIAS DE DADOS, MACHINE LEARNING, IA, BIOTECNOLOGIA(SEQUENCIAMENTO DE DNA) E ANALISE DE DADOS
+
+# LIST
+    # Listas em Python funcional como vetores/matrizes (arryas) de outras linguagens, com a diferença de serem *DINAMICAS e também de podermos colocar *QUALQUER tipo de dado.
+    
+    # - Dinamico: em C ou Java tamanho e tipo fixo
+    # - em Python NÃO tem tamanho fixo
+    # - em Python listas são representadas usando conchetes []
+
+lista1 = []
+lista2 = [1, 2, 3, 4, 5]
+lista3 = ["G", "e", "e", "k"]
+lista4 = list(range(11))
+lista5 = list("Geek University")
+lista6 = [1, 99, 50, 2, 1505, 27, 33, 1, 54, 99, 1]
+print(lista4)
+
+# Verificar se o valor esta contido na lista
+if 4 in lista2:
+    print('Encontrei o numero 4')
+else:
+    print('Não encontrei o numero 4')
+    
+# Ordenar lista de valores
+lista6.sort()
+print(f'lista numerica ordenada:  {lista6}') 
+    # Duvida? por que não pode fazer print(lista6.sort())
+    # Duvida? por que o retorno é none, equivale a null de js ?
+
+# Ordenar lista de string
+lista5.sort()
+print(lista5)
 
 # Contar itens repetidos na lista
 qtLetterE = lista5.count('e')
@@ -90,5 +123,3 @@ lista8 = lista3.copy() # Duvida? Por que usar copy e não atribuir direto ?
 # REVISÃO SLICE
 # lista[ start : endBeforeAt : step ]
 # range( start: endBeforeAt: step )
-
-# EXERCICIOS MODULO 4 E 5
