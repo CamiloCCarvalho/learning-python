394d005603a6d8550718862ad5a5069b8661b451
ash <ash@kali.localkali>
2023-05-22 20:13:23 -0300

    att - Exercicios Modulo 4 e 5

--- exercicies/week1/exMod4.py
+++ exercicies/week1/exMod4.py
@@ -1,5 +1,5 @@
 # MODULO 4 - EX01
-def showNumberInt():
+def show_number_int():
     try:
         number = int(input("Digite um número inteiro: "))
         print("O número digitado foi:", number)
@@ -7,7 +7,7 @@ def showNumberInt():
         print("Você não digitou um número inteiro!")
         
 # MODULO 4 - EX02
-def showNumberFloat():
+def show_number_float():
     try:
         number = float(input("Digite um número REAL: "))
         if int(number) == number:
@@ -17,7 +17,7 @@ def showNumberFloat():
         print(error)
         
 # MODULO 4 - EX03
-def sumNumbers():
+def sum_numbers():
     try:
         listNumber = list([int(input("Digite um numero:")),
                            int(input("Digite um numero:")),
@@ -30,7 +30,7 @@ def sumNumbers():
         print(e)
 
 # MODULO 4 - EX04
-def squareNumber():
+def square_numb():
     try:
         number = float(input('Digite um numero para saber o quadrado do mesmo:'))
         number = number**2
@@ -39,7 +39,7 @@ def squareNumber():
         print(e)
 
 # MODULO 4 - EX05
-def partNumber():
+def part_numb():
     try:
         number = float(input("Digite um número REAL: "))
         if int(number) == number and not '.':
@@ -48,3 +48,47 @@ def partNumber():
     except ValueError as error:
         print(error)
        
+# MODULO 4 - EX06
+def temp_c_to_f():
+    try:
+        tempc = float(input('Digite a temperatura em graus Celsius: '))
+        tempf = float(tempc * 1.8 + 32)
+        print(f'Temperatura em Fahrenheit: {tempf}')
+    except ValueError as error:
+        print(error)
+        
+# MODULO 4 - EX07
+def temp_f_to_c():
+    try:
+        tempf = float(input('Digite a temperatura em graus Fahrenheit: '))
+        tempc = float((tempf-32) * 1.8)
+        print(f'Temperatura em Celsius: {tempc}')
+    except ValueError as error:
+        print(error)
+
+# MODULO 4 - EX08  ##########
+def temp_k_to_c():
+    try:
+        tempk = float(input('Digite a temperatura em graus Kelvin: '))
+        tempc = float(tempk - 273)
+        print(f'Temperatura em Celsius: {tempc}')
+    except ValueError as error:
+        print(error)
+
+# MODULO 4 - EX09  ##########
+def temp_k_to_c():
+    try:
+        tempc = float(input('Digite a temperatura em graus Celsius: '))
+        tempk = float(tempc + 273)
+        print(f'Temperatura em Kelvin: {tempk}')
+    except ValueError as error:
+        print(error)
+
+# MODULO 4 - EX10  ##########
+def velocity_km_to_ms():
+    try:
+        veloc_km = float(input('Digite a Velocidade em Km/h: '))
+        veloc_ms = float(veloc_km/3.2)
+        print(f'Velocidade em M/s: {veloc_ms}')
+    except ValueError as error:
+        print(error)
--- exercicies/week1/exMod5.py
+++ exercicies/week1/exMod5.py
@@ -1,5 +1,5 @@
 import math
-
+import random
 # MODULO 5 - EX01
 def talestNumber():
     try:
@@ -53,5 +53,66 @@ def isOdd():
             print('O numero é impar')
     except ValueError:
         print('Error: Você NÃO digitou um numero INTEIRO!')
+print()
 
+# MODULO 5 - EX06
+print(" MODULO 5 - EXERCICIO 6")
+num1 = random.randint(-1000, 1000)
+num2 = random.randint(-1000, 1000)
+print(f'DADOS OS NUMEROS {num1} e {num2}')
 
+if num1 > num2:
+    print(f'O maior é: {num1} | diferença: {num1-num2}')
+elif num1 == num2:
+    print(" Numeros Iguais")
+else:
+    print(f'O maior é: {num2} | diferença: {num2-num1}')
+    
+# MODULO 5 - EX07
+print(" MODULO 5 - EXERCICIO 7")
+num1 = random.randint(-1000, 1000)
+num2 = random.randint(-1000, 1000)
+print(f'DADOS OS NUMEROS {num1} e {num2}')
+
+if num1 > num2:
+    print(f'O maior é: {num1} | diferença: {num1-num2}')
+elif num1 == num2:
+    print(" Numeros Iguais")
+else:
+    print(f'O maior é: {num2} | diferença: {num2-num1}')
+    
+print()
+
+# MODULO 5 - EX08
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+if (nota1 < 0.0 or nota1 > 10.0) and (nota2 < 0.0 or nota2 > 10.0):
+    print(f'confira as notas digitas NOTA1: {nota1} e NOTA2: {nota2} | AMBAS precisam estar entre 0.0 e 10.0')
+else:
+    print(f'A média do aluno é: {(nota1+nota2)/2}')
+   
+print() 
+    
+# MODULO 5 - EX09
+salario = float(input('Digite o valor do salario: '))
+prest = float(input('Digite o valor da prestação: '))
+
+if prest > (salario * 0.2):
+    print(f'Empréstimo não concedido. Prestação muito alta')
+else:
+    print(f'Emprestimo concedido')
+
+print()
+
+# MODULO 5 - EX10
+h = float(input('Digite a Altura: '))
+gen = str(input('Digite o sexo: "F" para Feminino / "M" para Masculino: '))
+
+if gen == 'F' or 'f':
+    print(f'Peso ideal Feminino: {round((62.1 * h) - 44.7, 2)} Kg')
+elif gen == 'M' or 'm':
+    print(f'Peso ideal Masculino: {round((72.7 * h) - 58, 2)} Kg')
+else:
+    print('Por favor confira os dados digitados!')
+    
