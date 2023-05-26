f293f79298869f9dacaad5e2828e1ac03e93101d
ash <ash@kali.localkali>
2023-05-26 18:19:07 -0300

    att

---
+++ curiosities/speed_test.py
@@ -0,0 +1,41 @@
+import itertools
+import threading
+import time
+import sys
+import speedtest
+
+done = False
+speed_test = speedtest.Speedtest()
+
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rTesting your (Download | Upload) ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print()
+    sys.stdout.write('\rDone!     ')
+
+
+t = threading.Thread(target=animate)
+t.start()
+
+
+#long process here
+print()
+download = speed_test.download() 
+upload = speed_test.upload()
+ping = speed_test.results.ping
+
+
+# RESULT
+print()
+print('______________________')
+print(f'DOWNLOAD: {download / (10**6):.2f} Mbps')
+print(f'UPLOAD: {upload / (10**6):.2f} Mbps')
+print(f'PING: {ping:.2f} ms')
+print('______________________')
+
+done = True
